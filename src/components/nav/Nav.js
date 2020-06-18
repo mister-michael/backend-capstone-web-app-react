56b875073dc3a345766db27a544f2f6eaaf241ae
Michael Clark <michaelclarknashville@protonmail.com>
2020-06-18 14:37:04 -0500

    nav bar links added, logout functional

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import './Nav.css'
+import AuthMethods from '../auth/AuthMethods'
 
 const Nav = props => {
 
+    const {logout} = AuthMethods()
+
+    const handleLogout = () => {
+        logout();
+        // props.history.push({pathname: "/login"})
+    }
+
     return (
-        <div>Nav</div>
+        <div className="nav-container">
+            <Link className="nav-link" to="/">PS</Link>
+            <Link className="nav-link" to="/equipment">EQ</Link>
+            <Link className="nav-link" to="/clients">CL</Link>
+            <Link className="nav-link" to="/staff">ST</Link>
+            <Link
+                className="nav-link"
+                to="/login"
+                onClick={handleLogout}>XX</Link>
+        </div>
     )
 };
 
-export default Nav
+export default withRouter(Nav)
