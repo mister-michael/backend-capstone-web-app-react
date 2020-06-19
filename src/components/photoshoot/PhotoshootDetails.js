c00e9a45ecde20f037902d7d833465bdd5f63070
Michael Clark <michaelclarknashville@protonmail.com>
2020-06-19 01:46:33 -0500

    photoshoots page styling night 1

--- src/components/photoshoot/PhotoshootDetails.js
+++ src/components/photoshoot/PhotoshootDetails.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ApiManager from '../../modules/ApiManager';
+import './Photoshoot.css'
 
 const PhotoshootDetails = props => {
+
+    const [photoshoot, setPhotoshoot] = useState([])
+
+    let indoorText = ""
+    // photoshoot.indoor : indoorText = "Indoor" d
+    const fetchPhotoshoot = () => {
+        ApiManager.getOne("photoshoots", props.photoshootId)
+            .then(res => {
+                console.log(photoshoot.client)
+                setPhotoshoot(res)
+            })
+    };
+
+    useEffect(() => {
+        fetchPhotoshoot()
+        console.log(photoshoot.first_name)
+    }, [])
     return (
-        <div>{props.photoshootId}</div>
+        <>
+                <div className="photoshoot-details-container">
+                    <div className="photoshoot-details-empty-div"><a className="psd-heading">{photoshoot.name}</a></div>
+                    <div className="photoshoot-details-div">{photoshoot.name}</div>
+                    <div className="photoshoot-details-div">{photoshoot.date_scheduled}</div>
+                    <div className="photoshoot-details-div">{photoshoot.location}</div>
+                    <div className="photoshoot-details-div">
+                        {photoshoot.indoor ? "Indoor" : "Outdoor"}</div>
+                    <div className="photoshoot-details-div">${photoshoot.charge}</div>
+                    {/* <div> {photoshoot.client.last_name}</div> */}
+                </div>
+        </>
     )
 }
 
--- src/components/photoshoot/PhotoshootListItem.js
+++ src/components/photoshoot/PhotoshootListItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Photoshoot.css'
+import './Photoshoot.css';
 
 const PhotoshootListItem = props => {
 
