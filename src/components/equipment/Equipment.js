ca58ef503d69c5b7f625bd75ef590ad24c5438f4
Michael Clark <michaelclarknashville@protonmail.com>
2020-06-23 20:50:24 -0500

    delete button added to Equipment page, and functioning

--- src/components/equipment/Equipment.js
+++ src/components/equipment/Equipment.js
@@ -5,15 +5,23 @@ import EquipmentListItem from './EquipmentListItem';
 const Equipment = props => {
 
     const [equipmentList, setEquipmentList] = useState([])
+    const [refresh, setRefresh] = useState(false)
 
     async function fetchEquipment () {
         await ApiManager.getAll("equipments")
         .then(res => setEquipmentList(res))
     };
 
+    const handleDelete = (evt) => {
+        const equipmentToDeleteId = parseInt(evt.target.id.split("--")[1])
+        ApiManager.delete("equipments", equipmentToDeleteId)
+        .then(()=>setRefresh(true))
+    }
+
     useEffect(()=>{
         fetchEquipment();
-    },[])
+        setRefresh(false)
+    },[refresh])
 
     return (
         <>
@@ -21,7 +29,15 @@ const Equipment = props => {
         id="add-equipment-button"
         onClick={()=>props.history.push('/equipment/form')}
         >Add Equipment</button>
-            {equipmentList.map(res => <EquipmentListItem equipment={res} from={"equipment-page"} {...props} key={res.id} />)}
+            {equipmentList.map(res => 
+            <>
+            <button
+            id={`delete-equipment-button--${res.id}`}
+            onClick={handleDelete}
+            >delete</button>
+            <EquipmentListItem equipment={res} from={"equipment-page"} {...props} key={res.id} />
+            </>
+            )}
         </>
     )
 };
